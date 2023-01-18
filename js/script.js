d308e31c918891157cdcbaf82aa90ddd4c4b4544
omid jalilian <omidjalilian147@gmail.com>
2023-01-18 13:11:38 +0330

    v2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,68 @@
-AOS.init();
-var splide = new Splide( '.splide', {
-    focus  : 'left',
-    type   : 'loop',
-    autoplay: true,
-    autoWidth: true,
-    perPage: 3,
-    perMove: 1,
-    padding: { left: 20, right: 20 },
-    gap: 20,
-    classes: {
-		pagination: 'splide__pagination hide',
-		page      : 'splide__pagination__page hide',
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 5000);
+}
+
+let tl = gsap.timeline({defaults: {ease: "inOut", duration: 1}})
+
+tl.to(".animE", {"clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)", "opacity": 1, y: 0, stagger: 0.2})
+tl.to(".anim1", {"clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)", "opacity": 1, y: 0, stagger: 0.2})
+
+gsap.to(".anim2", {
+    scrollTrigger: ".anim2",
+    "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+    opacity: 1,
+    y: 0,
+    stagger: 0.2,
+    duration: 1,
+    delay: 0.7,
+});
+
+gsap.to(".anim3", {
+    scrollTrigger: {
+        trigger: ".anim3",
+        toggleActions: "play none none none",
     },
-} );
-    
-splide.mount();
+    "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+    opacity: 1,
+    y: 0,
+    stagger: 0.2,
+    duration: 1,
+    delay: 0.7,
+});
+
+gsap.to(".anim4", {
+    scrollTrigger: {
+        trigger: ".anim4",
+        toggleActions: "play none none none",
+    },
+    "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+    opacity: 1,
+    y: 0,
+    stagger: 0.2,
+    duration: 1,
+    delay: 0.7,
+});
+
+gsap.to(".anim5", {
+    scrollTrigger: {
+        trigger: ".anim5",
+        toggleActions: "play none none none",
+    },
+    "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+    opacity: 1,
+    y: 0,
+    stagger: 0.2,
+    duration: 1,
+    delay: 0,
+});
--- scroll/app.js
+++ scroll/app.js
@@ -1,12 +1,11 @@
 new fullpage('#fullpage', {
     anchors: ['home', 'slide', 'designs', 'designs2', 'about'],
-    navigationTooltips: ['Home', 'Slide Show', 'Designs', 'About'],
+    navigationTooltips: ['Home', 'About', 'Painting', 'Painting', 'Contact'],
     autoScrolling: true,
-    sectionsColor: ['#000000', '#232323', '#22222A', '#232323'],
+    sectionsColor: ['#000000', "#1E1E1E", '#241924', '#212134', '#232323'],
     navigation: true,
     onLeave: (origin,destination,direction)=> {
         const section = destination.item;
-        console.log(section);
-        TweenMax.fromTo(section, 1, {opacity: 0.5}, {opacity: 1},'+=700');
+        gsap.from("." + section.className.split(" ")[1], {"duration": 0.7, opacity: 0})
     }
 });
